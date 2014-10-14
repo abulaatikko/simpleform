0e36df14fcf6a2366d125db072ce8e17b11ab426
Lassi Heikkinen <lassi@pulu.org>
2014-10-14 23:39:46 +0300

    change: form1.json restructured to flat format feature: form data is iterated in template to form elements

--- src/client/js/controllers.js
+++ src/client/js/controllers.js
@@ -1,6 +1,10 @@
-app.controller('FormCtrl', function($scope) {
+app.controller('FormCtrl', function($scope, $http) {
 
     function init() {
+        $scope.form = [];
+        var form = $http.get('/form/1').then(function(data) {
+            $scope.form = data.data;
+        });
     }
 
     init();
