655e9df38e6c619aecc98d11222f748ee22e7029
Lassi Heikkinen <lassi@pulu.org>
2014-10-15 21:13:31 +0300

    feature: show input fields according to nextSteps attribute. hiding is not implemebted

--- src/client/js/controllers.js
+++ src/client/js/controllers.js
@@ -7,6 +7,30 @@ app.controller('FormCtrl', function($scope, $http) {
         });
     }
 
+    $scope.showNextSteps = function(options, value) {
+        // first find nextSteps
+        var nextSteps = [];
+        for (var k = 0; k < options.length; k++) {
+            if (options[k].value == value) {
+                if (typeof options[k].nextSteps !== "undefined") {
+                    nextSteps = options[k].nextSteps;
+                }
+                k = options.length;
+            }
+        }
+
+        // todo: hide elements
+
+        // show the field
+        for (var i = 0; i < nextSteps.length; i++) {
+            for (var j = 0; j < $scope.form.length; j++) {
+                if ($scope.form[j].id == nextSteps[i]) {
+                    $scope.form[j].show = true;
+                }
+            }
+        }
+    };
+
     init();
 
 });
