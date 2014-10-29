6037175d9894c415d080e13f2ffe0d2556f346b7
LassiHeikkinen <lassi.heikkinen@gmail.com>
2014-10-29 22:31:27 +0200

    feature: index page backend test

--- test/unit/server/server.spec.js
+++ test/unit/server/server.spec.js
@@ -2,10 +2,20 @@ var request = require('request');
 var config = require('../../config');
 
 describe('server', function() {
+
     it("should response form json", function(done) {
         request('http://' + config.host + ':' + config.port + '/form/1', function(error, response) {
             expect(response.body).toMatch(/nextFields":\["money\-toolittle\-submit"\]\},\{"label":"I\'ve got too much/);
             done();
         });
     });
+
+    it("should response index page", function(done) {
+        request('http://' + config.host + ':' + config.port + '/', function(error, response) {
+            expect(response.body).toMatch(/Development/);
+            expect(response.body).toMatch(/div ui-view/);
+            done();
+        });
+    });
+
 });
